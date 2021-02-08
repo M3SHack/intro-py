b31f7b55969eadf4be841e03cc33e74eacacc86a
M3SHack <75744232+M3SHack@users.noreply.github.com>
2021-02-08 04:17:37 -0600

    Create ch3-things-todo.py
    Will come back to this after re-reading

---
+++ my-code/ch3-things-todo.py
@@ -0,0 +1,16 @@
+#///// Redo ch3 Todo
+
+quick_total = 60 * 60
+print(quick_total)
+
+seconds_per_minute = 60
+seconds_per_hour = 60
+
+seconds_per_day = seconds_per_minute * seconds_per_hour
+print(seconds_per_day)
+
+total_one = seconds_per_day / seconds_per_hour
+print(total_one)
+
+total_two = seconds_per_day // seconds_per_hour
+print(total_two)
