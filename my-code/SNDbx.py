7ef38c6549135de7703d6e6c7976832dc5c7210e
M3SHack <75744232+M3SHack@users.noreply.github.com>
2021-02-08 01:24:12 -0600

    End of Ch2 Exercises
    End of Ch2 (this is not Ch1) SMH

---
+++ my-code/SNDbx.py
@@ -0,0 +1,50 @@
+# a = 7
+# print(a)
+
+# b = a
+# print(b)
+
+# #///// type & isinstance examples
+# print(type(a))
+# print(isinstance(b, int))
+
+# #///// more literal values
+# a = 7
+# b = a
+# type(a)
+# type(b)
+# type(58)
+
+# #///// repeated from earlier example
+# y = 5
+# x = 12 - y
+# print(x)
+
+# #///// assigning to multiple names
+# two = deux = zwei = 2
+# print(deux)
+# print(zwei)
+# print(zwei + 3)
+
+# #///// copying 
+# x = 5
+# y = x
+# print(y)
+# x = 29
+# print(x)
+# print(y)
+
+# #///// changing mutable objects
+# a = [2, 4, 6]
+# b = a
+# print(a)
+# print(b)
+# print(a[0])
+# print(b[2])
+
+# #///// changing elements in a list
+# a[1] = 10000
+# print(a)
+# print(b)
+
+#///// END OF CHAPTER 1 Exercises
---
+++ my-code/ch1-things-todo.py
@@ -0,0 +1,4 @@
+prince = 99
+print(type(5))
+print(type(2.0))
+print(type(5 + 2.0))
