ee91f9e4fecf62d0b6b10e6cdba62e5e59116b07
M3SHack <75744232+M3SHack@users.noreply.github.com>
2021-02-14 01:33:10 -0600

    Create ch4-SNDbx.py

---
+++ my-code/ch4-SNDbx.py
@@ -0,0 +1,30 @@
+#/////Example One
+sum =0
+sum += 1
+sum += 2
+sum += 3
+sum += 4
+print(sum)
+
+#/////Example Two
+sum = 1 + \
+    2 + \
+    3 + \
+    4
+print(sum)
+
+#/////Example Three
+sum = (
+    1 +
+    2 +
+    3 +
+    4)
+print(sum)
+
+#/////Example Four
+disaster = True
+if disaster:
+    print("Whoa!")
+else:
+    print("Whee!")
+
