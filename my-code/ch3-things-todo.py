21ac303e1662af205279f24632127ab73e6dff6d
M3SHack <75744232+M3SHack@users.noreply.github.com>
2021-02-13 21:58:27 -0600

    re-did ch3-things-todo
    was kinda not understanding things due to tiredness. Had to make sure I came back and tried when I was awake.

--- my-code/ch3-things-todo.py
+++ my-code/ch3-things-todo.py
@@ -1,16 +1,53 @@
-#///// Redo ch3 Todo
-
-quick_total = 60 * 60
-print(quick_total)
-
+#How many seconds are in an hour?
 seconds_per_minute = 60
-seconds_per_hour = 60
+minutes_per_hour = 60
 
-seconds_per_day = seconds_per_minute * seconds_per_hour
-print(seconds_per_day)
+seconds_per_hour = seconds_per_minute * minutes_per_hour
+print(seconds_per_hour)
 
-total_one = seconds_per_day / seconds_per_hour
-print(total_one)
+hours_per_day = 24
+total_seconds_per_day = seconds_per_hour * hours_per_day
+print(total_seconds_per_day)
 
-total_two = seconds_per_day // seconds_per_hour
-print(total_two)
+print("\n")
+
+#/////Using floating-point / division
+decimal_answer = total_seconds_per_day / seconds_per_hour
+print(decimal_answer)
+
+#/////Using integer // division
+no_decimal_answer = total_seconds_per_day // seconds_per_hour
+print(no_decimal_answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#///// My old code is  below
+# #///// Redo ch3 Todo
+
+# quick_total = 60 * 60
+# print(quick_total)
+
+# seconds_per_minute = 60
+# seconds_per_hour = 60
+
+# seconds_per_day = seconds_per_minute * seconds_per_hour
+# print(seconds_per_day)
+
+# total_one = seconds_per_day / seconds_per_hour
+# print(total_one)
+
+# total_two = seconds_per_day // seconds_per_hour
+# print(total_two)
