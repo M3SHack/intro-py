9fbd0b6902772c7d870fee62fd63646ccb4a9944
M3SHack <75744232+M3SHack@users.noreply.github.com>
2021-02-14 03:39:29 -0600

    ch4-things-tod
    completed

---
+++ my-code/ch4-things-todo.py
@@ -0,0 +1,25 @@
+#/////Guessing game
+
+# secret = 5
+# guess = 7
+
+# if guess < secret:
+#     print("To low!")
+# elif guess == secret:
+#     print("Good guess!!")
+# else:
+#     print("To high!")
+
+#/////Choose the green veggitable
+green = False
+small = True
+
+if small:
+    if green:
+        print("It's a pea!")
+    else:
+        print("It's a cherry!")
+elif green:
+        print("It's a watermelon!")
+else:
+    print("It's not small or green, so who knows!")
